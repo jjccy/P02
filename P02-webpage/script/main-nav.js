1a59edc1887e4ef3a750d07a24da45a5140019aa
Jimmy <jimmychu82@gmail.com>
2020-06-11 20:59:00 -0700

    Main nav still visible without script

--- P02-webpage/script/main-nav.js
+++ P02-webpage/script/main-nav.js
@@ -1,33 +1,54 @@
 
-
+var appear = false;
 
 $(document).ready(function() {
+  changeNav();
+
+  $(".main-nav-left").css({
+    'visibility' : 'hidden'
+  });
+
   $("#call-left").click(function(event) {
     event.preventDefault();
+    appear = true;
+    changeNav();
 
-    if(!$('.main-nav-top').hasClass('scroll-up'))
-    {
-      $('.main-nav-top').addClass('scroll-up');
-    }
-    if(!$('.main-nav-left').hasClass('appear'))
-    {
-      $('.main-nav-left').addClass('appear');
-    }
     return false;
   });
 
   $("#close-left").click(function(event) {
     event.preventDefault();
+    appear = false;
+    changeNav();
 
+    return false;
+  });
+  
+});
+
+function changeNav() {
+  $(".main-nav-left").css({
+    'visibility' : 'visible'
+  });
+
+  if (appear) {
+    if(!$('.main-nav-top').hasClass('scroll-up'))
+    {
+      $('.main-nav-top').addClass('scroll-up');
+    }
+    if($('.main-nav-left').hasClass('disappear'))
+    {
+      $('.main-nav-left').removeClass('disappear');
+    }
+  }
+  else {
     if($('.main-nav-top').hasClass('scroll-up'))
     {
       $('.main-nav-top').removeClass('scroll-up');
     }
-    if($('.main-nav-left').hasClass('appear'))
+    if(!$('.main-nav-left').hasClass('disappear'))
     {
-      $('.main-nav-left').removeClass('appear');
+      $('.main-nav-left').addClass('disappear');
     }
-    return false;
-  });
-
-});
+  }
+};
