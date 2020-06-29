c3db8dbcf9fdb692e90050263b9b13f8647b2f1b
Jay <jayzhang0703@gmail.com>
2020-06-29 10:27:28 -0700

    stage styleguide files

---
+++ P02-Step1/P02-webpage/script/main-nav.js
@@ -0,0 +1,77 @@
+
+var appear = false;
+
+$(document).ready(function() {
+  changeNav();
+
+  $(".main-nav-left").css({
+    'visibility' : 'hidden'
+  });
+
+  $(".main-nav-item").css({
+    'opacity' : '0'
+  });
+
+  $("#call-left").click(function(event) {
+    event.preventDefault();
+    appear = true;
+    changeNav();
+
+    return false;
+  });
+
+  $("#close-left").click(function(event) {
+    event.preventDefault();
+    appear = false;
+    changeNav();
+
+    return false;
+  });
+
+});
+
+// $(window).scroll(function() {
+//   var height = $(window).scrollTop();
+//   var shade = 'linear-gradient(180deg, rgba(0,0,0,0.5) 0%, rgba(0,0,0,0) 100%)'
+//
+//   if (height > 100) {
+//     $('.main-nav-top').css({
+//       'background' : shade
+//     });
+//   } else {
+//     $('.main-nav-top').css({
+//       'background' : "none"
+//     });
+//   }
+//
+// });
+
+function changeNav() {
+  $(".main-nav-left, .main-nav-item").css({
+    'visibility' : 'visible'
+  });
+  $(".main-nav-item").css({
+    'opacity' : '1'
+  });
+
+  if (appear) {
+    if(!$('.main-nav-top').hasClass('scroll-up'))
+    {
+      $('.main-nav-top').addClass('scroll-up');
+    }
+    if($('.main-nav-left').hasClass('disappear'))
+    {
+      $('.main-nav-left').removeClass('disappear');
+    }
+  }
+  else {
+    if($('.main-nav-top').hasClass('scroll-up'))
+    {
+      $('.main-nav-top').removeClass('scroll-up');
+    }
+    if(!$('.main-nav-left').hasClass('disappear'))
+    {
+      $('.main-nav-left').addClass('disappear');
+    }
+  }
+};
